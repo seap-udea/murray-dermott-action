d0d4149c184816a9a2072f6523674adcb0b4fa3b
Jorge Zuluaga <jorge.zuluaga@udea.edu.co>
2023-05-16 10:00:04 -0500

    Update Python packages tutorial

--- tutorials/PythonPackages/pymiau-dist/setup.py
+++ tutorials/PythonPackages/pymiau-dist/setup.py
@@ -25,7 +25,7 @@ setuptools.setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         ],
-    version='0.3',
+    version='0.4',
 
     # ######################################################################
     # FILES
